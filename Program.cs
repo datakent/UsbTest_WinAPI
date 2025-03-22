0f8aef22d2026dc42af43edec04150db4fe9d00a
datakent <murat.turan@datakent.com>
2025-03-22 09:02:56 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,3 @@
-﻿//Rongta RPP320
-//Rongta Termal yazıcı ya bulk transfer ile veri gönderimi
-//Üreticinin sürücüleri veya 3prt bir sürücü/kütüphane kullanılmadan
-//direkt Windows API leri üzerinde veri gönderilecektir.
-
 using System;
 
 namespace UsbTest_WinAPI
@@ -27,31 +22,6 @@ namespace UsbTest_WinAPI
 
             p.save2Disk("D:/Output.hx");
 
-            #region devicePathComment
-            /*
-            Cihaz path bilgisi ile "Bulk Transfer" / raw data transferi
-
-            \\?\usb#vid_0fe6&pid_811e#6&4817b6d&0&2#{a5dcbf10-6530-11d2-901f-00c04fb951ed}
-            
-            vid_0fe6: Vendor ID (Üretici Kimliği), cihazın üreticisini temsil eder.
-            pid_811e: Product ID (Ürün Kimliği), cihaz modelini tanımlar.
-            6: Bağlandığı kökün (hub/root) bir indeksini gösterebilir.
-            4817b6d: Cihazın belirli oturumdaki "instance" kimliğidir. USB port değiştiğinde, tak çıkart vs.. durumlarda değişebilir
-            0&2: Fiziksel bağlantının yerini tanımlayan bir alt bilgi
-            GUID ise USB cihazları için kullanılan bir arayüz sınıfını temsil ediyor. Aşağıki adreslerde detay var
-              Bu bilgiler büyük/küçük harf duyarlı değil.
-            */
-
-            //her iki tanımlada erişim oluyor, fakat teknik dokümanlar
-            //"a5dcbf10..." olanı kullanmayı öneriyor! Zira ilki eski sürüm Windowslar zamanında. 2binli yıllar...
-            //
-            //https://learn.microsoft.com/en-us/windows-hardware/drivers/install/guid-class-usb-device
-            //string devicePath = @"\\.\\usb#vid_0fe6&pid_811e#6&4817b6d&0&2#{a6782bce-4376-4de2-8096-70aa9e8fed19}";
-
-            //https://learn.microsoft.com/en-us/windows-hardware/drivers/install/guid-devinterface-usb-device
-            //USBDeview InstanceID örneği: USB\VID_0FE6&PID_811E\6&197fd1e3&0&2
-            //Bu veriyi devicePath'a aktarırken \ -> # değişimi yapılmalı ve diğer kısımlar aynen kalmalı!
-            #endregion
             string devicePath = @"\\?\USB#VID_0FE6&PID_811E#6&197fd1e3&0&2#{a5dcbf10-6530-11d2-901f-00c04fb951ed}";
 
             IntPtr deviceHandle = WinAPI.CreateFile(devicePath,
